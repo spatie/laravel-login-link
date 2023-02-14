c05fe3972d2157380993f7345857fe87283dd69e
Luca Longo <l.longo@ambita.it>
2023-02-14 11:18:52 +0100

    Vue component

---
+++ resources/js/login-link.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { router } from '@inertiajs/vue3';
+
+const props = defineProps({
+    label: { type: String, default: () => 'Login' },
+    class: { type: String, default: () => 'underline' },
+    email: String,
+    key: String,
+    redirectUrl: String,
+    guard: String,
+    userAttributes: { type: Object, default: () => null },
+})
+
+function submit() {
+    router.post(route('loginLinkLogin'), {
+        email: props.email,
+        key: props.key,
+        redirect_url: props.redirectUrl,
+        guard: props.guard,
+        user_attributes: props.userAttributes,
+    })
+}
+</script>
+
+<template>
+    <form @submit.prevent="submit">
+        <button :class="props.class" type="submit">
+            {{ props.label }}
+        </button>
+    </form>
+</template>
