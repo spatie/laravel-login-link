80add1854ac27e476a31f8a790d529820ebc60a9
Florian <fouteox@gmail.com>
2024-06-22 23:12:33 +0200

    Add react support (jsx and tsx)

---
+++ resources/ts/LoginLink.tsx
@@ -0,0 +1,43 @@
+import React, { FormEvent } from 'react';
+import { router } from '@inertiajs/react'
+
+interface LoginLinkProps {
+    className?: string;
+    email?: string;
+    guard?: string;
+    keyId?: string;
+    label?: string;
+    redirectUrl?: string;
+    userAttributes?: Record<string, any>;
+}
+
+const LoginLink: React.FC<LoginLinkProps> = ({
+    className = 'underline',
+    email = null,
+    guard = null,
+    keyId = null,
+    label = 'Login',
+    redirectUrl = null,
+    userAttributes = null,
+}) => {
+    const submit = (event: FormEvent) => {
+        event.preventDefault();
+        router.post(route('loginLinkLogin'), {
+            email: email,
+            key: keyId,
+            redirect_url: redirectUrl,
+            guard: guard,
+            user_attributes: userAttributes,
+        });
+    };
+
+    return (
+        <form onSubmit={submit}>
+            <button className={className} type="submit">
+                {label}
+            </button>
+        </form>
+    );
+};
+
+export default LoginLink;
