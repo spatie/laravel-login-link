b3d279fb354de984a6945ad9b27987b49b93e2bf
yparitcher <y@paritcher.com>
2024-09-24 11:23:40 -0400

    Blade: use json_encode instead of @json
    @json breaks with attributes that have spaces

--- resources/views/loginLink.blade.php
+++ resources/views/loginLink.blade.php
@@ -6,7 +6,7 @@
         <input type="hidden" name="key" value="{{ $key }}">
         <input type="hidden" name="redirect_url" value="{{ $redirectUrl }}">
         <input type="hidden" name="guard" value="{{ $guard }}">
-        <input type="hidden" name="user_attributes" value=@json($userAttributes)>
+        <input type="hidden" name="user_attributes" value="{{ json_encode($userAttributes) }}">
 
         @include('login-link::loginLinkButton')
     </form>
